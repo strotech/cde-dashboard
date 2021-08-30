45d82dc0d4896048d18d9933d7540ff3b5b6693e
strotech <37307710+strotech@users.noreply.github.com>
2021-08-30 19:03:35 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+import './assets/styles/main.scss';
+
+import AdminLayoutContainer from "./components/containers/layouts/AdminLayoutContainer";
+
+import { withAuthenticator } from '@aws-amplify/ui-react'
+
+
+const App=()=>{
+ 
+  return(
+    <BrowserRouter>
+      <Switch>
+        <Route path="/admin" render={(props) => <AdminLayoutContainer {...props} />} />
+        <Redirect from="/" to="/admin/dashboard" />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default withAuthenticator(App);
+
+
