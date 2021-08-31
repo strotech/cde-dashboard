a0527e7c876883bafa5308da1f8a836f7707d510
strotech <37307710+strotech@users.noreply.github.com>
2021-08-31 17:42:48 +0530

    Update AboutPanel.js

--- src/components/panels/views/AboutPanel.js
+++ src/components/panels/views/AboutPanel.js
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react';
 import {API} from 'aws-amplify';
+import config from '../../../aws-exports';
+API.configure(config);
 const AboutSidebarPanel =()=> {
     useEffect(()=>{
       API.get('twittercovid','/twitter/covid').then(res=>console.log(res));
-      API.get('twitterflood','/twitter/flood').then(res=>console.log(res));
+      API.get('twittercovid','/twitter/flood').then(res=>console.log(res));
     },[]);
     return (
       <div>
