935ae65763dc07a0d4f03883cc585eeb9880c8a1
strotech <37307710+strotech@users.noreply.github.com>
2021-08-31 18:03:19 +0530

    Update index.js

--- amplify/backend/function/twittercovid/src/index.js
+++ amplify/backend/function/twittercovid/src/index.js
@@ -4,11 +4,10 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  }, 
+     headers: {
+         "Access-Control-Allow-Origin": "*",
+         "Access-Control-Allow-Headers": "*"
+     }, 
         body: JSON.stringify('Hello from Covid Lambda!'),
     };
     return response;
