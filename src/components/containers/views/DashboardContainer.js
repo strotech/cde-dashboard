0ff791a90c1e93e68c9680c3246d10db03578fda
DevX Prime <archerearthx@gmail.com>
2021-09-04 16:52:22 +0530

    Prototyped

--- src/components/containers/views/DashboardContainer.js
+++ src/components/containers/views/DashboardContainer.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React,{ useEffect }  from 'react';
+import {API} from 'aws-amplify';
+import config from '../../../aws-exports';
+
 import DashboardPanel from '../../panels/views/DashboardPanel'
 
+API.configure(config);
+
 const DashboardContainer =()=> {
+    useEffect(()=>{
+      API.get('twittercovid','/twitter/covid').then(res=>console.log(res));
+      API.get('twittercovid','/twitter/flood').then(res=>console.log(res));
+      API.get('twittercovid','/telegram/covid').then(res=>console.log(res));
+    },[]);
     return (
         <DashboardPanel />
     );
--- src/components/panels/views/AboutPanel.js
+++ src/components/panels/views/AboutPanel.js
@@ -1,13 +1,7 @@
-import React, { useEffect } from 'react';
-import {API} from 'aws-amplify';
-import config from '../../../aws-exports';
-API.configure(config);
+import React from 'react';
+
 const AboutSidebarPanel =()=> {
-    useEffect(()=>{
-      API.get('twittercovid','/twitter/covid').then(res=>console.log(res));
-      API.get('twittercovid','/twitter/flood').then(res=>console.log(res));
-      API.get('twittercovid','/telegram/covid').then(res=>console.log(res));
-    },[]);
+    
     return (
       <div>
           Hi DevX - About!!
